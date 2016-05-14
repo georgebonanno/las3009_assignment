b05b8cd37d515b6519d34d0962dc440c751666fa
georgebonanno <george.bonanno@gmail.com>
2016-05-14 10:38:50 +0200

    starting js in backoffice

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,17 @@
 'use strict';
 module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-watch');
-      grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-json-server');
 
   grunt.initConfig({
+    json_server: {
+      options: {
+        port: 13337,
+        hostname: '0.0.0.0',
+        db: "api/logo_commands.json", 
+      }
+    },
     watch: {
       src: {
         files: ['*.html','*.css','*.js'],
