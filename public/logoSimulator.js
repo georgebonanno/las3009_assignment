0c6b033a4f3e194f6b508eba531144214ec97677
georgebonanno <george.bonanno@gmail.com>
2016-05-21 11:53:35 +0200

    parsing of commands initated

--- public/logoSimulator.js
+++ public/logoSimulator.js
@@ -25,12 +25,80 @@ var logoCtrller=function($scope,commandRetrieval) {
 		}
 	}
 
+	var throwParseError=function(message) {
+		throw "parse error: "+message;
+	}
+
+	var evaluateFdStatement = function(words) {
+		if (words.length != 2) {
+			throwParseError("extra tokens in FD statement");
+		} else if (words[1].search(/^\d+$/) != 0) {
+			throwParseError("fd show have a number as argument and not "+words[i]);
+		} else {
+			return {
+				fd: (1*words[1]) 
+			}
+		}
+	}
+
+	var evaluateStatement=function(statement) {
+		var words=statement.split(" ");
+		console.log("to evaluated statement composed of "+words);
+		var evaluatedStatement = null;
+		if (words.length > 0 && words[0]) {
+			var command=words[0];
+			if (command.search(/^fd$/i) == 0) {
+				console.log("to evaluate fd statement")
+				evaluatedStatement=evaluateFdStatement(words);
+			} else {
+				throwParseError("unknown statement: "+command);
+			}
+		} else {
+			console.log("empty line found");
+		}
+		return evaluatedStatement;
+	}
+
+	var draw=function(evaluatedCommands) {
+		console.log("to evaluate");
+		for (var i in evaluatedCommands) {
+			for(var key in evaluatedCommands[i]) {
+				console.log("part "+key+" "+evaluatedCommands[i][key]);
+			}
+		}
+	}
+
+	var notifyOnExecution = function(success,message) {
+		if (success) {
+			logoCtrl.alertClass="alert-success";
+		} else {
+			logoCtrl.alertClass="alert-warning";
+		}
+		logoCtrl.execOutcomeMessage=message;
+	}
+
 	logoCtrl.evaluate=function (){
-		var canvas=document.getElementById('logoCanvas');
-		var ctx=canvas.getContext('2d');
-		ctx.fillStyle= "#FF0000";
-		ctx.fillRect(0,0,30,30);
-		ctx.stroke();
+		try {
+			var canvas=document.getElementById('logoCanvas');
+			var ctx=canvas.getContext('2d');
+			ctx.moveTo(0,0);
+			var commands=logoCtrl.currentCommands.split("\n");
+			var evaluatedCommands=[];
+			for(var i=0; i< commands.length; i++) {
+				var statement=commands[i];
+				if (statement) {
+					statement=statement.trim();
+					if (statement) {
+						var evalStat=evaluateStatement(statement); 
+						evalStat && evaluatedCommands.push(evalStat);
+					}
+				}
+			}
+			draw(evaluatedCommands);
+			notifyOnExecution(true,"commands evaluated successfully!");
+		} catch (e) {
+			notifyOnExecution(false,e);	
+		}
 	}
 };
 app.controller('LogoController',
