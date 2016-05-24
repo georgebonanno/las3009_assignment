39d496696018495f8562990d9bad03e1f4bd5cde
georgebonanno <george.bonanno@gmail.com>
2016-05-24 21:35:51 +0200

    drawing working from directive

---
+++ directives/showLogoDirective.js
@@ -0,0 +1,122 @@
+app.directive('showLogo',function() {
+
+	var controller = function() {
+
+		console.log("to draw = "+this.todraw);
+	};
+
+	function LogoDrawing(canvas) {
+
+		var ctx = canvas.getContext('2d');
+
+		function drawPosition(x,y) {
+			ctx.fillStyle = "#000000";
+			ctx.arc(x,y,5,0,2*Math.PI,false);
+			ctx.lineWidth=2;
+			ctx.strokeStyle="#003300";
+			ctx.stroke();
+		};
+
+		var currentPosition={
+				angle: 0,
+				x: 0,
+				y: 0,
+				pd: true
+			};
+
+		function init() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			currentPosition.angle=0;
+			currentPosition.x=canvas.width/2;
+			currentPosition.y=canvas.height/2;
+			currentPosition.pd=true;
+		}
+
+		function drawFd(length) {
+			var currentPoint=Geometry.makePoint(currentPosition.x,currentPosition.y);
+			var newPoint=Geometry.rotate(currentPoint,length,currentPosition.angle);
+
+			if (currentPosition.pd) {
+				Geometry.drawLine(ctx,currentPoint.x,currentPoint.y,newPoint.x,newPoint.y);
+			}
+			currentPosition.x=newPoint.x;
+			currentPosition.y=newPoint.y;
+		}
+
+		function drawLt(angle) {
+			currentPosition.angle+=-1*Math.radians(angle);
+		}
+
+		function drawRt(angle) {
+			currentPosition.angle+=Math.radians(angle);
+		}
+
+		function drawPenUpOrDown(penUpOrDown) {
+			if (penUpOrDown) {
+				currentPosition.pd = (penUpOrDown.search(/^pd$/i) == 0);
+			}
+		}
+
+		var draw=function(evaluatedCommands) {
+			console.log("to evaluate");
+			for (var cmdIdx in evaluatedCommands) {
+				var currentCommand=evaluatedCommands[cmdIdx];
+				if (currentCommand.fd) {
+					console.log("drawing fd "+currentCommand.fd);
+					drawFd(currentCommand.fd);
+				} else if (currentCommand.rt) {
+					drawRt(currentCommand.rt);
+				} else if (currentCommand.lt) {
+					drawLt(currentCommand.lt);
+				} else if (currentCommand.penDownOrUp) {
+					drawPenUpOrDown(currentCommand.penDownOrUp);
+				}
+			}
+
+			var pos=Geometry.makePoint(currentPosition.x,currentPosition.y);
+			Geometry.drawTriangle(ctx,pos,currentPosition.angle);
+			console.log("current position: "+currentPosition);
+		}
+
+		init();
+
+		return {
+			draw: draw
+		}
+
+	}
+
+	return {
+		restrict: 'E',
+		templateUrl: 'directives/show-logo.html',
+		scope: {
+			todraw: '@todraw'
+		},
+		link: function(scope, element, attrs) {
+            
+			scope.canvas = element.find('canvas')[0];
+			var logoDrawing = new LogoDrawing(scope.canvas);
+	        
+            scope.$watch("todraw", function(drawCommands) {
+            	if (drawCommands.search(/^\[.*\]$/) == 0) {
+            		var commandsStr=drawCommands.substring(1,drawCommands.length-1);
+            		var commands=commandsStr.split(",");
+
+            		var actualCommands = [];
+            		for (var i in commands) {
+            			actualCommands.push(JSON.parse(commands[i]));
+            		}
+            		console.log("commands = "+commands);
+        			logoDrawing.draw(actualCommands);
+            	} else if (typeof drawCommands === 'Array') {
+            		drawCommands.forEach(function(v) {
+            			console.log("element : "+v+" and logoDrawing = "+drawCommands);
+            		})
+            	} else {
+            		console.log("element is "+drawCommands);
+            	}
+            });
+        }
+
+	};
+});
--- public/logoSimulator.js
+++ public/logoSimulator.js
@@ -52,62 +52,6 @@ var logoCtrller=function($scope,commandRetrieval,commandParsing) {
 		}
 	}
 
-	function drawPosition(x,y) {
-		ctx.fillStyle = "#000000";
-		ctx.arc(x,y,5,0,2*Math.PI,false);
-		ctx.lineWidth=2;
-		ctx.strokeStyle="#003300";
-		ctx.stroke();
-	};
-
-
-	
-
-	function drawFd(length) {
-		var currentPoint=Geometry.makePoint(currentPosition.x,currentPosition.y);
-		var newPoint=Geometry.rotate(currentPoint,length,currentPosition.angle);
-
-		if (currentPosition.pd) {
-			Geometry.drawLine(ctx,currentPoint.x,currentPoint.y,newPoint.x,newPoint.y);
-		}
-		currentPosition.x=newPoint.x;
-		currentPosition.y=newPoint.y;
-	}
-
-	function drawLt(angle) {
-		currentPosition.angle+=-1*Math.radians(angle);
-	}
-
-	function drawRt(angle) {
-		currentPosition.angle+=Math.radians(angle);
-	}
-
-	function drawPenUpOrDown(penUpOrDown) {
-		if (penUpOrDown) {
-			currentPosition.pd = (penUpOrDown.search(/^pd$/i) == 0);
-		}
-	}
-
-	var draw=function(evaluatedCommands) {
-		console.log("to evaluate");
-		for (var cmdIdx in evaluatedCommands) {
-			var currentCommand=evaluatedCommands[cmdIdx];
-			if (currentCommand.fd) {
-				console.log("drawing fd "+currentCommand.fd);
-				drawFd(currentCommand.fd);
-			} else if (currentCommand.rt) {
-				drawRt(currentCommand.rt);
-			} else if (currentCommand.lt) {
-				drawLt(currentCommand.lt);
-			} else if (currentCommand.penDownOrUp) {
-				drawPenUpOrDown(currentCommand.penDownOrUp);
-			}
-		}
-
-		var pos=Geometry.makePoint(currentPosition.x,currentPosition.y);
-		Geometry.drawTriangle(ctx,pos,currentPosition.angle);
-		console.log("current position: "+currentPosition);
-	}
 
 	var notifyOnExecution = function(success,message) {
 		if (success) {
